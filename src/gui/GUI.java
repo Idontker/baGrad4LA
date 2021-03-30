df5c65f49ff548bf84f891c9676be85b60855e6e
idontker <idontker@users.noreply.github.com>
2021-03-31 01:54:42 +0200

    fixed minor stuff in some files

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,6 +1,5 @@
 package gui;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import gui.buttons.ListenerGoToPage;
--- src/util/Loader.java
+++ src/util/Loader.java
@@ -3,7 +3,6 @@ package util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
