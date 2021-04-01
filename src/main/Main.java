627571024c1f1b7c8db45db848acbd152f2d2455
idontker <idontker@users.noreply.github.com>
2021-04-01 14:45:50 +0200

    now actually reading the configs of all schools

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,23 +13,17 @@ public class Main {
 
         Loader loader = new Loader(path);
 
-        HashMap<String, Fach> faecher = loader.loadFaecher();
-
         Saver.construct(path);
 
-        
-        HashMap<String, Fach> dummy_realschule = new HashMap<String, Fach>();
-        dummy_realschule.put("Informatik", faecher.get("Informatik"));
-        
-        HashMap<String, Fach> dummy_mittelschule = new HashMap<String, Fach>();
-        dummy_mittelschule.put("Englisch", faecher.get("Englisch"));
-        
+        HashMap<String, Fach> gym = loader.loadFaecher("Gymnasium");
+        HashMap<String, Fach> real = loader.loadFaecher("Realschule");
+        HashMap<String, Fach> mittel = loader.loadFaecher("Mittelschule");
+
         HashMap<String, HashMap<String, Fach>> map = new HashMap<String, HashMap<String, Fach>>();
-        map.put("Gymnasium", faecher);
-        map.put("Realschule", dummy_realschule);
-        map.put("Mittelschule", dummy_mittelschule);
-        
-        
+        map.put("Gymnasium", gym);
+        map.put("Realschule", real);
+        map.put("Mittelschule", mittel);
+
         GUI g = new GUI(map);
         // g.update(); TODO
     }
