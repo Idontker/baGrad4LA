845840713f9484e00eaac398016129b7c02a4c9a
idontker <idontker@users.noreply.github.com>
2021-04-08 02:40:20 +0200

    added bestanden as an option

--- src/gui/NotenBox.java
+++ src/gui/NotenBox.java
@@ -11,9 +11,10 @@ public class NotenBox extends MyComboBox {
     private static final long serialVersionUID = 1L;
     private static final String[] noten = new String[] { "1.0", "1.3", "1.7", "2.0", "2.3", "2.7", "3.0", "3.3", "3.7",
             "4.0" };
+    private static final String[] bestehen = new String[] { "bestanden" };
 
     public NotenBox(Modul modul) {
-        super(noten, true);
+        super(chooseOptions(modul.gewicht), true);
 
         int i = indexOfGrad(modul.note) + 1;
 
@@ -27,9 +28,13 @@ public class NotenBox extends MyComboBox {
                 String selected = (String) getSelectedItem();
 
                 double newValue = 0.0;
-                try {
-                    newValue = Double.parseDouble(selected);
-                } catch (NumberFormatException nfe) {
+                if (Math.abs(modul.gewicht) < 0.001) {
+                    newValue = selected.equalsIgnoreCase("bestanden") ? 1.0 : 0.0;
+                } else {
+                    try {
+                        newValue = Double.parseDouble(selected);
+                    } catch (NumberFormatException nfe) {
+                    }
                 }
 
                 modul.note = newValue;
@@ -40,6 +45,10 @@ public class NotenBox extends MyComboBox {
             }
 
         });
+    }
+
+    private static String[] chooseOptions(double gewicht) {
+        return Math.abs(gewicht) < 0.001 ? bestehen : noten;
     }
 
     private int indexOfGrad(double d) {
